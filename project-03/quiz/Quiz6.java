90f864c49ede3cd8eed96339c3f3049a135e09e5
kim_mina <mina14712@gmail.com>
2024-03-29 12:35:00 +0900

    0329

--- project-03/quiz/Quiz6.java
+++ project-03/quiz/Quiz6.java
@@ -4,8 +4,8 @@ public class Quiz6 {
 	public static void main(String[] args) {
 		int num = 7;
 		int num2 = 10;
-		boolean result1 = (7%2 == 0);
-		boolean result2 = (10%2 == 0);
+		boolean result1 = (num%2 == 0);
+		boolean result2 = (num2%2 == 0);
 		
 		System.out.println("7은 홀수 입니까? "+result1);
 		System.out.println("10은 짝수 입니까? "+result2);
---
+++ project-04/conditional/Ex2.java
@@ -0,0 +1,18 @@
+package conditional;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age = 10;
+		if ( age >= 8 ) {
+			System.out.println("학교에 다닙니다");
+		} else {
+			System.out.println("학교에 다니지 않습니다");
+		}
+	}
+
+}
+// 디버깅 단축키 F6=>다음라인으로 이동
+// F8 => 다음 브레이크 포인트까지 스킵
